918748216287b6e9d806e40e54fd3eb530f28593
lucasAlex-sys <lucas.alex@hotmail.com.br>
2022-06-13 23:07:22 -0300

    ex1 e ex2: Done

--- tarefas.js
+++ tarefas.js
@@ -1,12 +1,40 @@
+const listTasks = document.querySelector("#lista-tarefas")
+const addTask = document.getElementById('incluir-nova-tarefa');
+const newTask = document.getElementById('nova-tarefa-nome');
+const newCategory = document.getElementById('nova-tarefa-categoria');
+const tasks = []
 class Task {
-    constructor(nome, categoria, realizada=false) {
-      this.nome = nome
-      this.categoria = categoria
-      this.realizada = realizada
-    }
+  constructor(nome, categoria, realizada=false) {
+    this.nome = nome
+    this.categoria = categoria
+    this.realizada = realizada
   }
+  adicionaNaPagina() {
+    const itens = document.createElement("li");
+    itens.classList.add("item-tarefa")
+    itens.classList.add(`categoria-${this.categoria}`)
+    itens.innerText = this.nome;
+    if (this.realizada)
+    {
+        itens.classList.add("marcado")
+    }
+    listTasks.appendChild(itens)
+  }
+}
+
+listTasks.innerHTML = "";
+const exemplo1 = new Task('Comprar laranja', 'compras',false)
+const exemplo2 = new Task('Escutar calypso', 'lazer', true)
+exemplo1.adicionaNaPagina()
+exemplo2.adicionaNaPagina()
+tasks.forEach(item => item.adicionaNaPagina());
+
+addTask.addEventListener('click', () => {
+  const task = new Task(newTask.value, newCategory.value);
+  tasks.push(task);
+  task.adicionaNaPagina();
+  newTask.value = "";
+  newTask.focus();
+});
+
   
-  const tasks = [
-    new Task('Comprar laranja', 'Compras',false),
-    new Task('Escutar calypso', 'Lazer', true)
-  ]
